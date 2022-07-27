e38e858f99dda008710ad12b9d7200c7e6099c37
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-07-27 21:23:02 +0800

    new: Cache last menu position

---
+++ src/main/java/me/flashyreese/mods/reeses_sodium_options/client/gui/Dim2iExtended.java
@@ -0,0 +1,15 @@
+package me.flashyreese.mods.reeses_sodium_options.client.gui;
+
+import me.jellysquid.mods.sodium.client.util.Dim2i;
+
+public interface Dim2iExtended {
+    void setX(int x);
+
+    void setY(int y);
+
+    void setWidth(int width);
+
+    void setHeight(int height);
+
+    boolean canFitDimension(Dim2i anotherDim);
+}
