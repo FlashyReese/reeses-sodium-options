33fa2687cc75ac945db13acb5614f1cef860b5a3
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-12-01 21:19:26 +0800

    Revert "change: Don't remap sodium classes"
    This reverts commit 0e77f75c88b31908220dc4d3c71f7f81ede671b4.

--- src/main/java/me/flashyreese/mods/reeses_sodium_options/mixin/sodium/MixinDim2i.java
+++ src/main/java/me/flashyreese/mods/reeses_sodium_options/mixin/sodium/MixinDim2i.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(value = Dim2i.class, remap = false)
+@Mixin(Dim2i.class)
 public abstract class MixinDim2i implements Dim2iExtended {
 
     @Shadow
