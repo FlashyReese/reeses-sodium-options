5e2d6a6c9ed9b2c4e58cc7607c247f12f77f8071
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2023-07-26 09:03:19 +0800

    change: Fix search bar text & navigation with search bar
    Fixed search bar text still displaying when containing user input.

--- src/main/java/me/flashyreese/mods/reeses_sodium_options/client/gui/frame/components/SearchTextFieldComponent.java
+++ src/main/java/me/flashyreese/mods/reeses_sodium_options/client/gui/frame/components/SearchTextFieldComponent.java
@@ -50,6 +50,7 @@ public class SearchTextFieldComponent extends AbstractWidget {
     private int firstCharacterIndex;
     private int selectionStart;
     private int selectionEnd;
+    private int lastCursorPosition = this.getCursor();
 
     public SearchTextFieldComponent(Dim2i dim, List<OptionPage> pages, AtomicReference<Text> tabFrameSelectedTab, AtomicReference<Integer> tabFrameScrollBarOffset, AtomicReference<Integer> optionPageScrollBarOffset, int tabDimHeight, SodiumVideoOptionsScreen sodiumVideoOptionsScreen, AtomicReference<String> lastSearch, AtomicReference<Integer> lastSearchIndex) {
         this.dim = dim;
@@ -71,7 +72,7 @@ public class SearchTextFieldComponent extends AbstractWidget {
         if (!this.isVisible()) {
             return;
         }
-        if (!this.isFocused()) {
+        if (!this.isFocused() && this.text.isEmpty()) {
             String key = "rso.search_bar_empty";
             Text text = Text.translatable(key);
             if (text.getString().equals(key))
@@ -457,7 +458,9 @@ public class SearchTextFieldComponent extends AbstractWidget {
                         } else {
                             this.moveCursor(1);
                         }
-                        return this.getCursor() != this.text.length();
+                        boolean state = this.getCursor() != this.lastCursorPosition && this.getCursor() != this.text.length() + 1;
+                        this.lastCursorPosition = this.getCursor();
+                        return state;
                     }
                     case GLFW.GLFW_KEY_LEFT -> {
                         if (Screen.hasControlDown()) {
@@ -465,7 +468,9 @@ public class SearchTextFieldComponent extends AbstractWidget {
                         } else {
                             this.moveCursor(-1);
                         }
-                        return this.getCursor() != 0;
+                        boolean state = this.getCursor() != this.lastCursorPosition && this.getCursor() != 0;
+                        this.lastCursorPosition = this.getCursor();
+                        return state;
                     }
                     case GLFW.GLFW_KEY_HOME -> {
                         this.setCursorToStart();
