7af06c26b9eb79def6cccc228c18cc599ea639e3
Yao Chung Hu <reeszrbteam@gmail.com>
2026-06-21 03:09:24 -0600

    change: Bump to `2.1.0` for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ val PARCHMENT_VERSION by extra { null }
 // https://semver.org/
 val MAVEN_GROUP by extra { "me.flashyreese.mods" }
 val ARCHIVE_NAME by extra { "reeses-sodium-options" }
-val MOD_VERSION by extra { "2.0.5" }
+val MOD_VERSION by extra { "2.1.0" }
 val SODIUM_VERSION by extra { "0.8.12+mc26.1.2" }
 
 allprojects {
