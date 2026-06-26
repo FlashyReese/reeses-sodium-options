8066a99871af23c0ea98912f21705b7d7066c85d
Yao Chung Hu <reeszrbteam@gmail.com>
2026-06-25 19:12:32 -0600

    change: Localize controller guide labels

--- common/src/main/java/me/flashyreese/mods/reeses_sodium_options/client/gui/control/ControlGuide.java
+++ common/src/main/java/me/flashyreese/mods/reeses_sodium_options/client/gui/control/ControlGuide.java
@@ -3,20 +3,20 @@ package me.flashyreese.mods.reeses_sodium_options.client.gui.control;
 import net.minecraft.network.chat.Component;
 
 public record ControlGuide(Input input, Component label) {
-    public static ControlGuide press(String label) {
-        return new ControlGuide(Input.PRESS, Component.literal(label));
+    public static ControlGuide press(Component label) {
+        return new ControlGuide(Input.PRESS, label);
     }
 
-    public static ControlGuide navigationLeftRight(String label) {
-        return new ControlGuide(Input.NAVIGATION_LEFT_RIGHT, Component.literal(label));
+    public static ControlGuide navigationLeftRight(Component label) {
+        return new ControlGuide(Input.NAVIGATION_LEFT_RIGHT, label);
     }
 
-    public static ControlGuide previousTab(String label) {
-        return new ControlGuide(Input.PREVIOUS_TAB, Component.literal(label));
+    public static ControlGuide previousTab(Component label) {
+        return new ControlGuide(Input.PREVIOUS_TAB, label);
     }
 
-    public static ControlGuide nextTab(String label) {
-        return new ControlGuide(Input.NEXT_TAB, Component.literal(label));
+    public static ControlGuide nextTab(Component label) {
+        return new ControlGuide(Input.NEXT_TAB, label);
     }
 
     public enum Input {
