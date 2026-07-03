caa4eb65eb05f2598d69962f149bd71ca704b565
Yao Chung Hu <reeszrbteam@gmail.com>
2026-07-02 19:17:36 -0600

    change: Add fallback rendering for custom Sodium controls
    Use native RSO rows only for known stock Sodium controls, and delegate custom controls to Sodium's own ControlElement renderer.

---
+++ common/src/main/java/me/flashyreese/mods/reeses_sodium_options/client/gui/SodiumWidgetDimensions.java
@@ -0,0 +1,7 @@
+package me.flashyreese.mods.reeses_sodium_options.client.gui;
+
+import net.caffeinemc.mods.sodium.client.util.Dim2i;
+
+public interface SodiumWidgetDimensions {
+    void rso$setDimensions(Dim2i dim);
+}
